498158f2f146a724576585a2b5d14445dd4f9676
sandrabosk <sandraboskovic@hotmail.com>
2018-04-30 19:26:15 -0400

    first commit

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,6 +4,50 @@ window.onload = function() {
   };
 
   function startGame() {
+    // console.log("starting the game");
+    createGameBoard();
+  }
+
+  function createGameBoard(){
+    // console.log("creating the game");
+    var theCanvas = document.getElementById("game-board");
+    var ctx = theCanvas.getContext("2d");
+
+    ctx.fillStyle = "green";
+    ctx.fillRect(0,0,500,600);
+    ctx.fillStyle = "grey";
+    //            x  y  width height
+    //            |  |   |    |
+    ctx.fillRect(50, 0, 400, 600);
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(60, 0, 10, 600);
+    //  (500-50-10-10)  y  width of white line
+    //            ^     ^   ^   -------------------->height of white line
+    //            |     |   |   |
+    ctx.fillRect(430,   0, 10, 600);
+
+
+    // dashed middle line:
+    ctx.lineWidth = "10";
+    //      height of the line
+    //               |    |---------> empty space between lines
+    ctx.setLineDash([40, 20]);
+    ctx.strokeStyle = "white";
+    // canvas width lineWidth
+    //        ^     ^
+    //        |     |
+    // 245= (500 - 10)/2
+    ctx.moveTo(245, 600);
+    ctx.lineTo(245, 0);
+    ctx.stroke();
+
+
 
   }
+
+
+
+
+
 };
