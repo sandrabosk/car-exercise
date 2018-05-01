efe9633d4c6a02203f293def2a34bf8596393462
sandrabosk <sandraboskovic@hotmail.com>
2018-04-30 21:08:12 -0400

    first commit

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,17 +1,26 @@
 window.onload = function() {
+
+  function interval(){
+    setInterval(updateCanvas, 100);
+  }
+
+
   document.getElementById("start-button").onclick = function() {
+    interval(); 
     startGame();
   };
 
   function startGame() {
     // console.log("starting the game");
     createGameBoard();
+    drawCar();
   }
+
+  var theCanvas = document.getElementById("game-board");
+  var ctx = theCanvas.getContext("2d");
 
   function createGameBoard(){
     // console.log("creating the game");
-    var theCanvas = document.getElementById("game-board");
-    var ctx = theCanvas.getContext("2d");
 
     ctx.fillStyle = "green";
     ctx.fillRect(0,0,500,600);
@@ -41,10 +50,115 @@ window.onload = function() {
     ctx.moveTo(245, 600);
     ctx.lineTo(245, 0);
     ctx.stroke();
+    // score:
+    ctx.font = "50px Helevetica";
+    ctx.fillStyle = "pink";
+    //                   score x   y 
+    ctx.fillText("Score: "+ board.score, 0, 50 );
+  }
 
+
+  var carImage = new Image();
+  carImage.src = "images/car.png";
+
+  var car = {
+    width: 50,
+    height: 80,
+    // 220 = (245- (50/2))
+    x:220,
+    // 520 = canvas.height(600) - car.height(80)
+    y: 520,
+    moveLeft: function(){
+      // console.log("x before: ", this.x)
+      if(this.x > 60){
+        this.x -=10;
+      }
+      // console.log("x after: ", this.x)
+    },
+    moveRight: function(){
+      if(this.x < 400){
+        this.x +=10
+      }
+    }
+  }
+
+  function drawCar(){
+      ctx.drawImage(carImage, car.x, car.y, car.width, car.height);
+  }
+
+  var myObstacles = [];
+  var board = {
+    score: 0,
+    frames: 0
+  }
+
+  document.onkeydown = function(e){
+    if(e.keyCode === 37){
+      car.moveLeft();
+    } else if(e.keyCode === 39){
+      car.moveRight();
+    } else {
+      console.log("What are you doing?")
+    }
+
+    createGameBoard();
+    drawCar();
+    for(var i = 0; i < myObstacles.length; i++){
+      myObstacles[i].createObstacle();
+      myObstacles[i].y +=10;
+    }
+  }
+
+  function Obstacle(x, y, width, height){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.createObstacle = function(){
+      ctx.fillStyle = "yellow";
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+
+  }
+
+  function updateCanvas(){
+    ctx.clearRect(0, 0, 500, 600);
+    createGameBoard();
+    drawCar();
+    board.frames ++;
+    // give me a new obstacle every 60 frames and start now
+    // if this was equal to 0, we would have to wait 60 frames to see
+    // our first obstacle
+    if (board.frames % 60 === 1){
+      obstacleX = 60 + Math.floor(Math.random() * 300);
+      obstacleY = 0;
+      obstacleWidth = 100;
+      obstacleHeight = 20;
+      myObstacles.push(new Obstacle(obstacleX, obstacleY, obstacleWidth, obstacleHeight));  
+    }
+   
+    for(var i = 0; i < myObstacles.length; i++){
+      // console.log(myObstacles)
+      myObstacles[i].createObstacle();
+      myObstacles[i].y += 10;
+    }
+
+    if(myObstacles[i].y > 600){
+      myObstacles.splice(i, 1);
+      board.score++;
+    }
 
 
   }
+
+
+
+
+
+
+
+ 
+
 
 
 
